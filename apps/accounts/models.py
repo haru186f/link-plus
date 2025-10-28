0923aba035d2228d5012251b2edd11903f15f4a8
Haru <194200695+haru186f@users.noreply.github.com>
2025-10-28 18:17:33 +0900

    バスモデル（Bus）を追加

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# 学部
+# 学部（例：ITカレッジ）
 class Faculty(models.Model):
     name = models.CharField(max_length=100)
 
@@ -9,7 +9,7 @@ class Faculty(models.Model):
         return self.name
 
 
-# 学科
+# 学科（例：情報処理科）
 class Department(models.Model):
     name = models.CharField(max_length=100)
     faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE, related_name='departments')
@@ -18,7 +18,7 @@ class Department(models.Model):
         return self.name
 
 
-# コース
+# コース（例：システム開発コース）
 class Course(models.Model):
     name = models.CharField(max_length=100)
     department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name='courses')
@@ -26,6 +26,15 @@ class Course(models.Model):
     def __str__(self):
         return self.name
 
+
+# バス停（例：八王子みなみ野）
+class Bus(models.Model):
+    BUS_CHOICES = [('hachiouji', '八王子'), ('minamino', '八王子みなみ野')]
+    name = models.CharField(max_length=20, choices=BUS_CHOICES, default='hachiouji')
+    def __str__(self):
+        return self.name
+
+# カスタムユーザー
 class CustomUser(AbstractUser):
     GRADE_CHOICES = [(i, f"{i}年") for i in range(1, 5)]
     ROLE_CHOICES = [(1, '学生'), (2, '教員'), (3, '管理者')]
@@ -37,6 +46,7 @@ class CustomUser(AbstractUser):
     updated_at = models.DateTimeField(auto_now=True)
 
     course = models.ForeignKey('Course', on_delete=models.SET_NULL, null=True, blank=True)
+    bus = models.ForeignKey('Bus', on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.username
