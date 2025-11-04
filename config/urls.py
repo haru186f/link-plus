20e478b400bd814f3c3866e4287ab53a94037a1e
Haru <194200695+haru186f@users.noreply.github.com>
2025-11-04 15:08:24 +0900

    バス時刻のスクレイピングとstaticの適用

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('apps.core.urls')),
@@ -12,3 +13,4 @@ urlpatterns = [
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
