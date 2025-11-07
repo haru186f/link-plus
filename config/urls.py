ef081a287c5a06c21f26296b5c7f2aac30a220dc
haru186f <194200695+haru186f@users.noreply.github.com>
2025-11-07 15:06:06 +0900

    Refactor (#21)
    * 各種アプリの作成
    
    * apschedulerからdjango-crontabに変更
    
    * appsの構造の修正
    
    * コンフリクトを解決

--- config/urls.py
+++ config/urls.py
@@ -4,10 +4,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include('apps.home.urls')),                # ホーム
+    path('', include('apps.home.urls')),        # その他
     path('lecture/', include('apps.lecture.urls')),     # 講義
     path('news/', include('apps.news.urls')),        # お知らせ
     path('bus/', include('apps.bus.urls')),         # バス時刻
+    path('accounts/', include('apps.accounts.urls')),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path('admin/', admin.site.urls),
 ]
 
 if settings.DEBUG:
