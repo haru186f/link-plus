97b4b006212e046c703a4ea0a42b39ab5e0793aa
haru186f <194200695+haru186f@users.noreply.github.com>
2025-11-07 14:02:56 +0900

    appsの構造を修正 (#19)
    * 各種アプリの作成
    
    * apschedulerからdjango-crontabに変更
    
    * appsの構造の修正

--- apps/bus/models.py
+++ apps/bus/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from apps.accounts.models import Bus
 
-# Create your models here.
+class BusSchedule(models.Model):
+    DIRECTION_CHOICES = [
+        ('to_school', '登校'),
+        ('from_school', '下校'),
+    ]
+
+    campus_departure = models.TimeField(blank=True, null=True)                 # キャンパス発
+    station_departure = models.TimeField(blank=True, null=True)                # 駅発
+    campus_arrival = models.TimeField(blank=True, null=True)                   # キャンパス着
+    note = models.CharField(max_length=100, blank=True, null=True)             # 備考
+
+    direction = models.CharField(max_length=20, choices=DIRECTION_CHOICES)     # 登校 or 下校
+    is_saturday = models.BooleanField(default=False)                           # 平日 or 土曜日
+
+    bus = models.ForeignKey(Bus, on_delete=models.CASCADE)                     # 八王子 or 八王子みなみ野
--- config/urls.py
+++ config/urls.py
@@ -4,10 +4,19 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+<<<<<<< HEAD
+    path('', include('apps.home.urls')),        # その他
+    path('lecture/', include('apps.lecture.urls')),     # 講義
+    path('news/', include('apps.news.urls')),        # お知らせ
+    path('bus/', include('apps.bus.urls')),         # バス時刻
+||||||| 80b9e94
+    path('', include('apps.core.urls')),
+=======
     path('', include('apps.core.urls')),        # その他
     path('', include('apps.lecture.urls')),     # 講義
     path('', include('apps.news.urls')),        # お知らせ
     path('', include('apps.bus.urls')),         # バス時刻
+>>>>>>> origin/main
     path('accounts/', include('apps.accounts.urls')),
     path("accounts/", include("django.contrib.auth.urls")),
     path('admin/', admin.site.urls),
