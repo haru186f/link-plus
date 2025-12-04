e24f3416ec4d1c6121adef661f32571561743179
Haru <194200695+haru186f@users.noreply.github.com>
2025-12-04 10:34:34 +0900

    コンフリクトを解決

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -19,13 +19,7 @@ urlpatterns = [
     path('webhook/', receive_email_webhook, name='email_webhook'),
     path('api/body/<int:pk>/', api_email_body, name='api_email_body'),
     path('api/lecture-events/', lecture_events, name='lecture_events'),
-<<<<<<< HEAD
     path('api/bus-schedules/', views.get_data_for_modal, name='get_modal_data'),
-    path("api/bus/next/", GetNextBusInfo.as_view(), name="api_next_bus")
-||||||| parent of 687a929 (feat: ニュース一覧画面にホームに戻るボタンを追加)
-    path("api/bus/next/", GetNextBusInfo.as_view(), name="api_next_bus")
-=======
     path("api/bus/next/", GetNextBusInfo.as_view(), name="api_next_bus"),
-    path("api/debug-bus/", DebugBusSchedule.as_view(), name="debug_bus")
->>>>>>> 687a929 (feat: ニュース一覧画面にホームに戻るボタンを追加)
+    path("api/debug-bus/", DebugBusSchedule.as_view(), name="debug_bus"),
 ]
